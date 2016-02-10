3c6f7d963178283e283339b09860892e8bcd5d03
Alexandr Babenko <huktoDev@mini-alexandr.dlink>
2016-02-10 22:15:53 +0500

    Initial Commit

---
+++ FastenTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastenTestProject
+//
+//  Created by Alexandr Babenko on 10.02.16.
+//  Copyright © 2016 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FastenTestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastenTestProject
+//
+//  Created by Alexandr Babenko on 10.02.16.
+//  Copyright © 2016 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
